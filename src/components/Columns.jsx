7ffadc6669c02ece1ff32db85f312d83d114cf38
Leyley1115 <sophia.pris22@gmail.com>
2025-11-23 21:20:39 +0300

    Разделение на компоненты

---
+++ src/components/Columns.jsx
@@ -0,0 +1,56 @@
+import {Card_1_1, Card_1_2, Card_1_3, Card_1_4, Card_1_5, Card_2_1, Card_3_1, Card_3_2, Card_3_3, Card_4_1, Card_5_1} from "./Cards";
+
+function Columns(){
+    return(
+        <>
+        <div className="main__column column">
+			<div className="column__title">
+				<p>Без статуса</p>
+			</div>
+			<div className="cards">
+				<Card_1_1 />
+				<Card_1_2 />
+				<Card_1_3 />
+				<Card_1_4 />
+				<Card_1_5 />
+			</div>
+		</div>						
+		<div className="main__column">
+			<div className="column__title">
+				<p>Нужно сделать</p>
+			</div>
+			<div className="cards">
+				<Card_2_1 />
+			</div>
+		</div>
+		<div className="main__column">
+			<div className="column__title">
+				<p>В работе</p>
+			</div>
+			<div className="cards">
+				<Card_3_1 />
+				<Card_3_2 />
+				<Card_3_3 />
+			</div>
+		</div>
+		<div className="main__column">
+			<div className="column__title">
+				<p>Тестирование</p>
+			</div>
+			<div className="cards">
+				<Card_4_1 />
+			</div>
+		</div>
+		<div className="main__column">
+			<div className="column__title">
+				<p>Готово</p>
+			</div>
+			<div className="cards">
+				<Card_5_1 />			
+			</div>
+		</div>
+        </>
+    )
+}
+
+export default Columns;
---
+++ src/components/Main_f.jsx
@@ -0,0 +1,19 @@
+import Columns from "./Columns";
+
+function Main(){
+    return (
+        <main className="main">
+			<div className="container">
+				
+				<div className="main__block">
+					<div className="main__content">
+						<Columns />
+					</div>
+				
+				</div>
+			</div>
+		</main>
+    )
+};
+
+export default Main;
