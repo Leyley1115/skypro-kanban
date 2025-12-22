716ff8592cd3ec7d9eb5e27aa0263de3e8d98dba
Leyley1115 <sophia.pris22@gmail.com>
2025-12-22 04:21:06 +0300

    замена на индивидуальный токен

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -8,25 +8,23 @@ import { Outlet } from 'react-router-dom'
 import { useEffect, useState, useCallback } from 'react'
 
 export function MainPage({isAuth}){
+  const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+  const token = userInfo?.token;
   const [loading, setLoading] = useState(false);
   const [cards, setCards] = useState([]);
   const [error, setError] = useState('');
   const getCards = useCallback(async () => {
     try {
       setLoading(true);
-      const data = await fetchCards({
-      // пока у нас не реализована авторизация, передаём токен вручную
-      token: 'bgc0b8awbwas6g5g5k5o5s5w606g37w3cc3bo3b83k39s3co3c83c03ck',
-      });
+      const data = await fetchCards({token});
       console.log('API data:', data.tasks);
       if (data) setCards(data.tasks);
       } catch (err) {
-        // console.log('ОШИБКА ПОПАЛА В CATCH:', err);
         setError(err.message);
       } finally {
         setLoading(false);
       }
-  }, []);
+  }, [token]);
 
   useEffect(() => {
       getCards();
